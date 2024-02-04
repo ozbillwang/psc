201318071cdb261f23d18c2b70b56f392edce797
Bill Wang <bill.wang>
2024-02-04 14:56:31 +1100

    cors fix

--- password-checker-backend/server.js
+++ password-checker-backend/server.js
@@ -5,14 +5,12 @@ const axios = require('axios');
 const app = express();
 
 const cors = require('cors');
-//app.use(cors()); // This will allow all domains. For production, configure allowed origins.
+app.use(cors()); // This will allow all domains. For production, configure allowed origins.
 
 app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
-    } else {
-        cors();
-    }
+    } 
 });
 
 app.options('*',cors())// include before other routes
